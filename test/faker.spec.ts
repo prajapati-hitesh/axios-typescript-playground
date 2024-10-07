98488c65b56ce67e0e4bbc365bc1e00f3e377c2f
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2024-10-07 12:00:53 +0530

    localized faker tests added

--- test/faker.spec.ts
+++ test/faker.spec.ts
@@ -1,11 +1,11 @@
-import { faker } from '@faker-js/faker';
+import { fakerEO } from '@faker-js/faker';
 
 describe('Faker Suite', () => {
     it('should print first name 10 times', async () => {
         const counter: number = 10;
 
-        for(let i = 0; i< counter; i++) {
-            console.log(faker.person.firstName('male'));
-        }        
+        for (let i = 0; i < counter; i++) {
+            console.log(fakerEO.phone.number({ style: 'international' }));
+        }
     })
-})
+});
